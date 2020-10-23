70ca1399183e637c94a0d55bbaa378d50d59368e
HarrisonStrand <harrisonstrand@gmail.com>
2020-10-23 13:24:53 -0700

    add rows and columns for result text and images

--- js/scripts.js
+++ js/scripts.js
@@ -4,15 +4,21 @@ $(document).ready(function(){
     const musicStr = $("input:radio[name=music]:checked").val();
     const music = parseInt(musicStr);
     
-    const beverageStr = $("input:radio[name=music]:checked").val();
+    const beverageStr = $("input:radio[name=beverage]:checked").val();
     const beverage = parseInt(beverageStr);
     
-    const activityStr = $("input:radio[name=music]:checked").val();
+    const activityStr = $("input:radio[name=activity]:checked").val();
     const activity = parseInt(activityStr);
 
     const result = music + beverage + activity;
 
-    
+    if (result >=5 && result <=7) {
+      $("")
+    } else if (result >=10 && result <=12) {
+
+    } else if (result >=13 && result <=15) {
+
+    } else (result ===8 || result ===9)
 
 
     event.preventDefault();
