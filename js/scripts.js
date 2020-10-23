92e82b85315818e4efa7d95c1ba76e8296c0e2a9
HarrisonStrand <harrisonstrand@gmail.com>
2020-10-23 12:05:26 -0700

    add variables for input in javascript and convert them into integers

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,20 @@
 $(document).ready(function(){
   $("button#submit").click(function(){
 
-    const 
+    const musicStr = $("input:radio[name=music]:checked").val();
+    const music = parseInt(musicStr);
+    
+    const beverageStr = $("input:radio[name=music]:checked").val();
+    const beverage = parseInt(beverageStr);
+    
+    const activityStr = $("input:radio[name=music]:checked").val();
+    const activity = parseInt(activityStr);
+
+    const result = music + beverage + activity;
+
+    
 
 
-
-
-
+    event.preventDefault();
   });
 });  
