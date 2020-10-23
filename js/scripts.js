c16cfff50ac096f67eff931e57c429371a092f94
HarrisonStrand <harrisonstrand@gmail.com>
2020-10-23 13:54:56 -0700

    add text for result answers and radio buttons

--- js/scripts.js
+++ js/scripts.js
@@ -3,23 +3,43 @@ $(document).ready(function(){
 
     const musicStr = $("input:radio[name=music]:checked").val();
     const music = parseInt(musicStr);
-    
+
     const beverageStr = $("input:radio[name=beverage]:checked").val();
     const beverage = parseInt(beverageStr);
     
     const activityStr = $("input:radio[name=activity]:checked").val();
     const activity = parseInt(activityStr);
+    
+    const distractionStr = $("input:radio[name=distraction]:checked").val();
+    const distraction = parseInt(distractionStr);
 
-    const result = music + beverage + activity;
+    const result = music + beverage + activity + distraction;
 
-    if (result >=5 && result <=7) {
-      $("")
+    if (result >=4 && result <=6) {
+      $(".cSharp").show();
+      $(".java").hide();
+      $(".ruby").hide();
+      $(".go").hide();
+
+    } else if (result >=7 && result <=9) {
+      $(".cSharp").hide();
+      $(".java").show();
+      $(".ruby").hide();
+      $(".go").hide();
+      
     } else if (result >=10 && result <=12) {
-
-    } else if (result >=13 && result <=15) {
-
-    } else (result ===8 || result ===9)
-
+      $(".cSharp").hide();
+      $(".java").hide();
+      $(".ruby").show();
+      $(".go").hide();
+      
+    } else {
+    $(".cSharp").hide();
+    $(".java").hide();
+    $(".ruby").hide();
+    $(".go").show();
+    
+    }
 
     event.preventDefault();
   });
